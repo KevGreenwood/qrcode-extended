f133688c731488ed0c6ad8c3f79e82e3b7d95b94
Kevin Greenwood <kevinrecalde324@outlook.com>
2024-12-16 11:44:24 -0500

    Update commit

--- PillowMethod.py
+++ PillowMethod.py
@@ -27,33 +27,32 @@ def style_outer_eyes(img):
   draw.rectangle((img_size-90, 60, img_size-60, 90), fill=0) #top right eye
   draw.rectangle((60, img_size-90, 90, img_size-60), fill=0) #bottom left eye
   return mask
+
 import qrcode
 from qrcode.image.styledpil import StyledPilImage
 from qrcode.image.styles.moduledrawers import RoundedModuleDrawer,VerticalBarsDrawer,SquareModuleDrawer
 from  qrcode.image.styles.colormasks import SolidFillColorMask
 
-if not hasattr(PIL.Image, 'Resampling'):
-  PIL.Image.Resampling = PIL.Image
-# Now PIL.Image.Resampling.BICUBIC is always recognized.
 
 
 qr = qrcode.QRCode(error_correction=qrcode.constants.ERROR_CORRECT_H)
 
-qr.add_data('http://www.medium.com')
+qr.add_data("MECARD:N:{lastname_txt.value},{name_txt.value};NICKNAME:{nickname_txt.value};TEL:{work_phone_txt.value};TEL:{priv_phone_txt.value};TEL:{phone_txt.value};EMAIL:{mail_txt.value};BDAY:{self.birthday};URL:{url_txt.value}NOTE:{filled_txt.value};ADR:,,{street_txt.value},{city_txt.value},{state_txt.value},{zip_txt.value},{country_txt.value};;")
 
 qr_inner_eyes_img = qr.make_image(image_factory=StyledPilImage,
                             eye_drawer=RoundedModuleDrawer(radius_ratio=1),
-                            color_mask=SolidFillColorMask(back_color=(255, 255, 255), front_color=(63, 42, 86)))
+                            color_mask=SolidFillColorMask(back_color=(1, 255, 1), front_color=(63, 42, 86)))
 
 qr_outer_eyes_img = qr.make_image(image_factory=StyledPilImage,
                             eye_drawer=VerticalBarsDrawer(),
-                            color_mask=SolidFillColorMask(front_color=(255, 128, 0)))
+                            color_mask=SolidFillColorMask(back_color=(1, 1, 1), front_color=(255, 128, 0)))
 
 qr_img = qr.make_image(image_factory=StyledPilImage,
-                       module_drawer=SquareModuleDrawer())
+                       module_drawer=SquareModuleDrawer(), color_mask=SolidFillColorMask(back_color=(1, 0, 0), front_color=(255, 255, 255)))
 
 inner_eye_mask = style_inner_eyes(qr_img)
 outer_eye_mask = style_outer_eyes(qr_img)
+
 intermediate_img = Image.composite(qr_inner_eyes_img, qr_img, inner_eye_mask)
 final_image = Image.composite(qr_outer_eyes_img, intermediate_img, outer_eye_mask)
 final_image.save('final_eyes_image.png')
